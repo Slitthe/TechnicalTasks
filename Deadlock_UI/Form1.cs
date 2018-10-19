3e47bc30c534f6b859a7a9e0c72a42c53459c125
Silviu Gherman <sil.gherman@gmail.com>
2018-10-19 13:46:06 +0300

    Fixed Deadlock example

--- Deadlock_UI/Form1.cs
+++ Deadlock_UI/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
@@ -14,11 +16,35 @@ namespace Deadlock_UI
 {
     public partial class Form1 : Form
     {
+        public string ResOne = "resource one";
+        public string ResTwo = "resource two";
+
+        public string GetSomeRes()
+        {
+            return "Some res";
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
 
 
+        public async Task DoSomethingAsync()
+        {
+            await Task.Delay(1000).ConfigureAwait(false);
+
+            await Task.Run(
+                async () =>
+                {
+                    await DoSomethingAsync().ConfigureAwait(false);
+                }    
+            ).ConfigureAwait(false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DoSomethingAsync().Wait();
+        }
     }
 }
